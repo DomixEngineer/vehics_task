b39b1b6a6c029277518066f04b60e22ed08503ce
DominikIT <Dominik.Dziewulski@wp.pl>
2023-03-29 23:02:02 +0200

    Backend counting calculations

--- vehics-app/app/Http/Controllers/CalculationsController.php
+++ vehics-app/app/Http/Controllers/CalculationsController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class CalculationsController extends Controller {
+
+    protected $netValue;
+    protected $grossValue;
+    protected $vat = 23;
+    protected $carYear;
 
     /**
      * Test method to testing API's
@@ -14,28 +21,94 @@ class CalculationsController extends Controller {
     /**
      * Netto to brutto calculations
      */
-    public function NetToGross($netto, $vat = 23) {
-        $brutto = $netto * (1 + ($vat / 100));
-        return $brutto;
+    public function calculateNetToGross(Request $request) {
+        $grossValue = $request->input('netPrice') / (1 + ($this->vat / 100));
+        $this->grossValue = round($grossValue, 2);
+        return $this->grossValue;
     }
 
     /**
      * Brutto to Netto calculations
      */
-    function GrossToNet($brutto, $vat = 23) {
-        $netto = $brutto / (1 + ($vat / 100));
-        return $netto;
+    function calculateGrossToNet(Request $request) {
+        $netValue = $request->input('grossPrice') / (1 + ($this->vat / 100));
+        $this->netValue = round($netValue, 2);
+        return $this->netValue;
     }
 
     public function calculateAcOc(Request $request) {
+
+        $carNetValue = $request->input('netPrice');
+        $carGrossValue = $request->input('grossPrice');
+        $carYearProduction = $request->input('yearProduction');
+        $carGpsIncluded = $request->input('gpsIncluded');
+        $carDivideInInstallments = $request->input('divideInInstallments');
+        $carCountOfInstallments = 0;
+        $ratioValue = 0;
+        if ($carDivideInInstallments == 1) {
+            $carCountOfInstallments = $request->input('countOfInstallments');
+        }
+
+        // Secure car over pricing
+        if ($carNetValue > 400000) {
+            return "Uwaga: samochód jest zbyt drogi, nie można wyliczyć składki."; 
+        }
+
+        // Set the proper car ratio value
+        if ($carNetValue < 40000) {
+            $ratioValue = 8;   // percentage value
+        } elseif ($carNetValue >= 40000 && $carNetValue < 100000) {
+            $ratioValue = 5; // percentage value
+        } elseif ($carNetValue >= 100000 && $carNetValue < 200000) {
+            $ratioValue = 4; // percentage value
+        } elseif ($carNetValue >= 200000 && $carNetValue < 400000) {
+            $ratioValue = 2; // percentage value
+        }
         
-        $carValue = $request->input('car_value');
-        $carYear = $request->input('car_year');
-        $gps = $request->input('gps', true);
-        $installments = $request->input('installments', 1);
-        $nettoValue = $request->input('netto_value');
+        // Add ratio if car is used
+        if ($carYearProduction <= 2021) {
+            $ratioValue += 1;
+        }
 
+        // Increment ratio if GPS has to be included
+        if ($carGpsIncluded) {
+            $ratioValue *= 1.11;
+        }
 
+        // contribution price
+        $contributionPrice = round($carNetValue * $ratioValue / 100, 2);
+
+        // Count additional ratio value if rates are supposed to be
+        if ($carCountOfInstallments == 2) {
+            $contributionPrice *= 1.02;
+            $ratingPricing = 200;
+        } elseif ($carCountOfInstallments == 4) {
+            $contributionPrice *= 1.04;
+            $ratingPricing = 200;
+        } else {
+            $ratingPricing = 0;
+        }
+
+        // Count the price of contribution rate
+        if ($carCountOfInstallments > 1) {
+            $rateOfContributionPrice = round(($contributionPrice + $ratingPricing) / $carCountOfInstallments, 2);
+        } else {
+            $rateOfContributionPrice = $contributionPrice;
+        }
+
+        $results = [
+            'net_price' => $carNetValue,                                // Cena netto
+            'gross_price' => $carGrossValue,                            // Cena brutto
+            'year_of_production' => $carYearProduction,                 // Rok produkcji
+            'gps_included' => $carGpsIncluded,                          // Czy GPS ma być w samochodzie
+            'ratio_value' => $ratioValue,                               // Współczynnik
+            'contribution_price' => $contributionPrice,                 // Składka
+            'rating_pricing' => $ratingPricing,                         // Opłaty ratowe
+            'count_of_installments' => $carCountOfInstallments,         // Liczba rat
+            'rate_of_contribution_price' => $rateOfContributionPrice    // Rata składki
+        ];
+
+        return $results;
     }
 
 }
--- vehics-app/resources/vueApp/components/HelloVue.vue
+++ vehics-app/resources/vueApp/components/HelloVue.vue
@@ -2,25 +2,53 @@
   <form @submit.prevent="calculateRates()">
     <div>
         <p>Cena netto</p>
-        <input type="text" v-model="premiumCalculator.nettoPrice">
+        <input 
+            type="number" 
+            step='0.01'
+            v-model="premiumCalculator.netPrice"
+            @change="calculateGross()"
+        >
     </div>
     <div>
         <p>Cena brutto</p>
-        <input type="text" v-model="premiumCalculator.nettoPrice">
+        <input 
+            type="number"
+            step='0.01'
+            v-model="premiumCalculator.grossPrice"
+            @change="calculateNetto()"
+        >
     </div>
     <div>
         <p>Rok produkcji samochodu</p>
-        <input type="text" v-model="premiumCalculator.nettoPrice">
+        <input type="text" v-model="premiumCalculator.yearProduction">
     </div>
     <div>
         <p>Czy GPS ma być zawarty</p>
-        <input type="text" v-model="premiumCalculator.nettoPrice">
+        <input type="checkbox" v-model="premiumCalculator.gpsIncluded">
     </div>
     <div>
-        <p>Na ile rat podzielić</p>
-        <input type="text" v-model="premiumCalculator.nettoPrice">
+        <p>Czy dzielić składkę na raty</p>
+        <select name="divide_in_installments" v-model="premiumCalculator.divideInInstallments">
+            <option value="1">Tak</option>
+            <option value="0">Nie</option>
+        </select>
+        <template v-if="premiumCalculator.divideInInstallments == 1">
+            <p>Na ile rat rozłożyć</p>
+            <select name="rates_count" v-model="premiumCalculator.countOfInstallments">
+                <option value="2">2 raty</option>
+                <option value="4">4 raty</option>
+            </select>
+        </template>
     </div>
-    <button>OBLICZ</button>
+    <template v-if="premiumCalculator.netPrice > 400000">
+        Nie obliczamy
+    </template>
+    <template v-else>
+        <button>OBLICZ</button>
+    </template>
+    <template>
+        WYNIK: {{ ocAcPrice }}
+    </template>
   </form>
 </template>
 
@@ -31,12 +59,15 @@ export default {
     name: 'HelloVue',
     data() {
         return {
+            ocAcPrice: '',
+            vat: 23,
             premiumCalculator: {
-                nettoPrice: '',
+                netPrice: '',
                 grossPrice: '',
                 yearProduction: '',
                 gpsIncluded: true,
-                ratesCount: 1
+                divideInInstallments: 0,
+                countOfInstallments: 2
             }
         }
     },
@@ -47,8 +78,23 @@ export default {
                     console.log(data.data)
                 })
         },
+        // Calculate dynamiclly Netto price (from gross value)
+        calculateNetto() {
+            axios.post('/api/calculate-gross-to-net', this.premiumCalculator)
+                .then((data) => {
+                    this.premiumCalculator.netPrice = data.data;
+                });
+        },
+        // Calculate dynamiclly Gross price (from Net value)
+        calculateGross() {
+            axios.post('/api/calculate-net-to-gross', this.premiumCalculator)
+                .then((data) => {
+                    this.premiumCalculator.grossPrice = data.data;
+                });
+        },
+        // Final AC / OC Calculations
         calculateRates() {
-            axios.post('/api/calculate', this.premiumCalculator)
+            axios.post('/api/calculate-ac-oc', this.premiumCalculator)
                 .then((data) => {
                     console.log(data.data)
                 })
